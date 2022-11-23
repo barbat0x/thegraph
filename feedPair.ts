f9d2f72d74f8a27a415a355c7be2237b29aad51c
flqrian <florian.chardel@gmail.com>
2022-11-23 10:25:35 +0100

    first commiy

---
+++ feedPair.ts
@@ -0,0 +1,13 @@
+class BlockParser {
+
+  
+    scanHistory(): void {
+        // get latest block
+        // filter from block to latest
+        // continue to scan
+    }
+  
+  }
+  
+  const reptile = new BlockParser();
+  // start scanning to last block
